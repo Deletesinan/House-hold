448ee348dac2fe93af7065cad4e767dc75edc39f
sinan ahamad <sinankoovale786@gmail.com>
2024-07-26 16:21:54 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,51 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var isValid = true;
+
+  var Fname = document.getElementById("Fname").value;
+  var Lname = document.getElementById("Lname").value;
+  var email = document.getElementById("email").value;
+  var Password = document.getElementById("Password").value;
+  var Message = document.getElementById("Message").value;
+
+ 
+  document.getElementById("Fname_error").textContent = "";
+  document.getElementById("Lname_error").textContent = "";
+  document.getElementById("email_error").textContent = "";
+  document.getElementById("Password_error").textContent = "";
+  document.getElementById("Message_error").textContent = "";
+
+  if (Fname.trim() === "") {
+      isValid = false;
+      document.getElementById("Fname_error").textContent = "First name is required";
+  }
+
+  if (Lname.trim() === "") {
+      isValid = false;
+      document.getElementById("Lname_error").textContent = "Last name is required";
+  }
+
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (email.trim() === "") {
+      isValid = false;
+      document.getElementById("email_error").textContent = "Email is required";
+  } else if (!emailPattern.test(email)) {
+      isValid = false;
+      document.getElementById("email_error").textContent = "Invalid email format";
+  }
+
+  if (Password.trim() === "") {
+      isValid = false;
+      document.getElementById("Password_error").textContent = "Password is required";
+  }
+
+  if (Message.trim() === "") {
+      isValid = false;
+      document.getElementById("Message_error").textContent = "Message is required";
+  }
+
+  if (isValid) {
+      alert("Form submitted successfully!");
+
+  }
+});
